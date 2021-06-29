45739ce5b06085b282991d67e6a85a94b914f25c
Natalia Kazakova <17986517+natakazakova@users.noreply.github.com>
2021-06-29 18:15:35 +0300

    Update Form1.cs

--- CS/Dashboard_CustomSchemaProvider/Form1.cs
+++ CS/Dashboard_CustomSchemaProvider/Form1.cs
@@ -1,11 +1,8 @@
-﻿using DevExpress.DashboardWin;
+using DevExpress.DashboardWin;
 
-namespace Dashboard_CustomSchemaProvider
-{
-    public partial class Form1 : DevExpress.XtraEditors.XtraForm
-    {
-        public Form1()
-        {
+namespace Dashboard_CustomSchemaProvider {
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm {
+        public Form1() {
             InitializeComponent();
             dashboardDesigner1.CreateRibbon();
             dashboardDesigner1.CustomizeDashboardTitle += DashboardDesigner1_CustomizeDashboardTitle;
@@ -13,19 +10,15 @@ namespace Dashboard_CustomSchemaProvider
             dashboardDesigner1.LoadDashboard(@"Data\dashboard-with-products.xml");
         }
 
-        private void DashboardDesigner1_CustomizeDashboardTitle(object sender, DevExpress.DashboardWin.CustomizeDashboardTitleEventArgs e)
-        {
-            DashboardToolbarItem itemSave = new DashboardToolbarItem(
-                (args) => UseCustomDBSchemaProvider())
-            {
+        private void DashboardDesigner1_CustomizeDashboardTitle(object sender, DevExpress.DashboardWin.CustomizeDashboardTitleEventArgs e) {
+            DashboardToolbarItem itemSave = new DashboardToolbarItem((args) => UseCustomDBSchemaProvider()) {
                 Caption = "Use Custom DBSchema Provider",
             };
             e.Items.Clear();
             e.Items.Add(itemSave);
         }
 
-        private void UseCustomDBSchemaProvider()
-        {
+        private void UseCustomDBSchemaProvider()         {
             dashboardDesigner1.CustomDBSchemaProviderEx = new CustomDBSchemaProvider();
         }
     }
